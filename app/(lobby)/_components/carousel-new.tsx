f6e6b29adf7ab07a40cdb48d7b95191e6b31bf95
jakisundays <jacobguillermooo@gmail.com>
2024-07-25 02:50:57 -0300

    [Optimize] Lobby performance

--- app/(lobby)/_components/carousel-new.tsx
+++ app/(lobby)/_components/carousel-new.tsx
@@ -29,10 +29,6 @@ export default function CarouselNew() {
           <br />
           crea tu Alter Ego
         </motion.p>
-        {/* <button className="px-4 py-2 backdrop-blur-sm border bg-emerald-300/10 border-emerald-500/20 text-white mx-auto text-center rounded-full relative mt-4">
-          <span>Join now →</span>
-          <div className="absolute inset-x-0  h-px -bottom-px bg-gradient-to-r w-3/4 mx-auto from-transparent via-emerald-500 to-transparent" />
-        </button> */}
       </motion.div>
     </ImagesSlider>
   );
