5c7d44f673448df2ee05e8c936f6385aad69160e
jakisundays <jacobguillermooo@gmail.com>
2024-07-25 03:13:30 -0300

    [Optimize] Lobby performance

--- app/(lobby)/_components/carousel-new.tsx
+++ app/(lobby)/_components/carousel-new.tsx
@@ -24,7 +24,7 @@ export default function CarouselNew() {
         }}
         className="z-50 flex flex-col justify-center items-center"
       >
-        <motion.p className="font-bold text-xl md:text-6xl text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 py-4">
+        <motion.p className="font-bold text-xl md:text-6xl text-center text-white py-4">
           Encuentra tu otra mitad
           <br />
           crea tu Alter Ego
