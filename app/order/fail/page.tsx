dd0eff550ba3bff2e3cd2304d5f5ce7c27da5ee5
jakisundays <jacobguillermooo@gmail.com>
2024-07-26 20:30:11 -0300

    [Feature] Mercado Pago V0

--- app/order/fail/page.tsx
+++
@@ -1,8 +0,0 @@
-'use client'
-import { usePathname } from 'next/navigation'
-const OrderFailure = () => {
-    const pathname = usePathname()
-    return <p>Current pathname: {pathname}</p>
-}
-
-export default OrderFailure;
--- app/order/pending/page.tsx
+++
@@ -1,8 +0,0 @@
-'use client'
-import { usePathname } from 'next/navigation'
-const OrderPending = () => {
-    const pathname = usePathname()
-    return <p>Current pathname: {pathname}</p>
-}
-
-export default OrderPending;
