b5b2a25819068230f85d777656ed284ad833b82d
jakisundays <jacobguillermooo@gmail.com>
2024-08-02 20:56:00 -0300

    [Chore] Log payment

--- app/api/payment/options/route.ts
+++ app/api/payment/options/route.ts
@@ -1,7 +1,12 @@
 import { NextRequest } from "next/server";
 
 export async function POST(request: NextRequest) {
-  const body = await request.json();
-  console.log({ body });
-  return new Response("ok", { status: 200 });
+  try {
+    const body = await request.json();
+    console.log({ body });
+    return new Response("ok", { status: 200 });
+  } catch (error) {
+    console.error("Error parsing JSON or processing request:", error);
+    return new Response("Internal Server Error", { status: 500 });
+  }
 }
